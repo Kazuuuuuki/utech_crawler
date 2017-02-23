382f8cd7ea8fa4e3e69920cd1cc2a921429ec9fc
kazuuuuuki <kazuki7932@gmail.com>
2017-02-23 09:48:55 +0000

    qiita crawler start

--- utech_crawler/utech_crawler/spiders/qiita_spider.py
+++ utech_crawler/utech_crawler/spiders/qiita_spider.py
@@ -10,12 +10,16 @@ class QiitaSpiderSpider(scrapy.Spider):
         urls = [
             'http://qiita.com/IshitaTakeshi/items/4607d9f729babd273960',
         ]
+        custome_setting = {
+        'DOWNLOAD_DELAY': 10
+        }
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+
     def parse(self, response):
         page = response.url.split("/")[-1]
-        filename = 'quotes-%s.html' % page
+        filename = 'qiita-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
