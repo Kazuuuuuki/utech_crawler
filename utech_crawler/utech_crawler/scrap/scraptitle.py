991a9c11b9b29a1330d69db905a0983801a73e74
yutatakaba <yuta.t.uni@gmail.com>
2017-03-02 08:45:46 +0000

    ファイル修正

--- utech_crawler/utech_crawler/scrap/scraptitle.py
+++ utech_crawler/utech_crawler/scrap/scraptitle.py
@@ -1,15 +1,12 @@
 import urllib.request as req
 import sys
 from bs4 import BeautifulSoup
-class ScrapLinks:
+class ScrapTitle:
 
         def __init__(self,url):
                 self.url = url
                 res = req.urlopen(url)
                 soup = BeautifulSoup(res, "html.parser")
-                links_list = soup.select('a[href^="http://qiita"]')
-                self.href = []
-                for a in links_list:
-                        self.href.append = a.attrs['href']
+                self.title = soup.find("title")
         def output(self):
-                print(self.href)
+                print (self.title.string)
