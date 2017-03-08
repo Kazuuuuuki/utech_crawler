6525138afc5863773589df58f45905bd7b01c7e1
yutatakaba <yuta.t.uni@gmail.com>
2017-03-08 03:41:35 +0000

    add file

---
+++ utech_crawler/utech_crawler/scrap/scrap_good.py
@@ -0,0 +1,31 @@
+import urllib.request as req
+import sys
+from bs4 import BeautifulSoup
+
+class ScrapGood:
+
+    def __init__(self,url):
+        self.url = url
+        res = req.urlopen(url)
+        soup = BeautifulSoup(res, "html.parser")
+        good = []
+        link = []
+        self.select_link = []
+        good_list = soup.find_all("ul" , class_="ItemLink__status")
+        links_list = soup.select(".media > div.media__body > div.ItemLink__title > a")
+        for a in links_list:
+            link.append(a.attrs['href'])
+        for num in range(20):
+            ward = str([good_list[num]])
+            good_list[num].span.decompose()
+            if "fa-comment-o" in ward:
+                good_list[num].a.decompose()
+                good.append(good_list[num].text)
+            else:
+                good.append(good_list[num].text)
+            dict_link = {good[num]:link[num]}
+            number = int(good[num])
+            if number > 100:
+                self.select_link.append(dict_link[good[num]])
+    def output(self):
+        print(self.select_link)
